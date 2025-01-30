825eccb9443725e7696ba250e45ea4e8789102a9
Emma ogar <indaboski42@gmail.com>
2025-01-31 00:04:47 +0100

    hi bye

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -7,7 +7,7 @@ import { PinContainer } from "./ui/Pin";
 
 const RecentProjects = () => {
   return (
-    <div className="py-8">
+    <div>
       <h1 className="heading">
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
